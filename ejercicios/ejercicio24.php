b9cfd076369c9579245d07054cb734db66b34ff6
rubhermar96 <rubhermar96@gmail.com>
2020-11-03 18:56:52 +0100

    asfas

--- ejercicios/ejercicio24.php
+++ ejercicios/ejercicio24.php
@@ -10,9 +10,22 @@
     <?php
     $columnas = 0;
     $aleatorio=0;
+    $numeros[]=0;
+    $numeros[]=0;
+    $numeros[]=0;
+    $numeros[]=0;
+    $numeros[]=0;
+    $numeros[]=0;
     echo'<tr>';
     for($columnas=0;$columnas<=6;$columnas++){
-        $aleatorio= 
+        $aleatorio= rand(1,49);
+        for($i=0;$i<6;$i++){
+            while($numeros[$i]==$aleatorio){
+                $aleatorio=rand(1,49);
+            }
+        }
+        $numeros[$columnas]= $aleatorio;
+        echo '<td align="center">'.$aleatorio.'</td>';
     }
     echo'</tr>';
     ?>
