5e16a9358d906007227b735ad8564e9034e819d7
rubhermar96 <rubhermar96@gmail.com>
2020-11-24 18:15:21 +0100

    log

--- proyecto/acceso/login.php
+++ proyecto/acceso/login.php
@@ -2,30 +2,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<title>Sign in</title>
+  	<link rel="stylesheet" type="text/css" href="style.css">
+  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
+	<nav>
+    	<div class="inicio">
+        	<a href="index.php"><span><i class="material-icons">home</i>Inicio</span></a>
+    	</div>
+	</nav>
+  	<div class="header">
+  		<h2>Iniciar Sesión</h2>
+  	</div>
+  	<form method="post" action="login.php">
+  		<?php include('errors.php'); ?>
   	<div class="input-group">
-  		<label>Username</label>
+  		<label>Nombre Usuario</label>
   		<input type="text" name="username" >
   	</div>
   	<div class="input-group">
-  		<label>Password</label>
+  		<label>Contraseña</label>
   		<input type="password" name="password">
   	</div>
   	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
+  		<button type="submit" class="btn" name="login_user">Entrar</button>
   	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
+  	<p>¿No eres Cliente? <a href="register.php">Regístrate</a></p>
+  	</form>
+  	<footer>
+        <p>Proyecto Desarrollo Web Entorno Servidor - Rubén Herrera Marcos</p>
+    </footer>
 </body>
 </html>
--- proyecto/acceso/register.php
+++ proyecto/acceso/register.php
@@ -2,38 +2,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<title>Sign up</title>
+  	<link rel="stylesheet" type="text/css" href="style.css">
+  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-  <div class="header">
-  	<h2>Register</h2>
-  </div>
+	<nav>
+    	<div class="inicio">
+        	<a href="index.php"><span><i class="material-icons">home</i>Inicio</span></a>
+    	</div>
+	</nav>
+  	<div class="header">
+  		<h2>Registrarse</h2>
+  	</div>
 	
-  <form method="post" action="register.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Email</label>
-  	  <input type="email" name="email" value="<?php echo $email; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Password</label>
-  	  <input type="password" name="password_1">
-  	</div>
-  	<div class="input-group">
-  	  <label>Confirm password</label>
-  	  <input type="password" name="password_2">
-  	</div>
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_user">Register</button>
-  	</div>
-  	<p>
-  		Already a member? <a href="login.php">Sign in</a>
-  	</p>
-  </form>
+  	<form method="post" action="register.php">
+  		<?php include('errors.php'); ?>
+  		<div class="input-group">
+  	  		<label>Nombre Usuario</label>
+  	  		<input type="text" name="username" value="<?php echo $username; ?>">
+  		</div>
+  		<div class="input-group">
+  	  		<label>Email</label>
+  	  		<input type="email" name="email" value="<?php echo $email; ?>">
+  		</div>
+  		<div class="input-group">
+  	  		<label>Contraseña</label>
+  	  		<input type="password" name="password_1">
+  		</div>
+  		<div class="input-group">
+  	  		<label>Confirmar Contraseña</label>
+  	  		<input type="password" name="password_2">
+  		</div>
+  		<div class="input-group">
+  	  		<button type="submit" class="btn" name="reg_user">Registrar</button>
+  		</div>
+  		<p>¿Ya eres Cliente? <a href="login.php">Inicia Sesión</a></p>
+  	</form>
+  	<footer>
+        <p>Proyecto Desarrollo Web Entorno Servidor - Rubén Herrera Marcos</p>
+    </footer>
 </body>
 </html>
