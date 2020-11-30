a463becef7bc23a2f1807e5e23660753d9ed3dc7
rubhermar96 <71779444+rubhermar96@users.noreply.github.com>
2020-11-30 23:50:06 +0100

    perfil admins

--- proyecto/admins/perfilAdmin.php
+++ proyecto/admins/perfilAdmin.php
@@ -31,9 +31,39 @@ if (isset($_GET['logout'])) {
     </div>
     <div class="titlePage">
       <h2>Perfil Administrador</h2>
+      <form method="post" action="perfilAdmin.php" class="formPerfil">
+        <?php if (isset($_SESSION['success'])) : ?>
+          <div class="error success" >
+      	    <h3>
+              <?php 
+          	    echo $_SESSION['success']; 
+          	    unset($_SESSION['success']);
+              ?>
+      	    </h3>
+          </div>
+  	    <?php endif ?>
+        <table>
+          <tbody>
+            <tr><td>Usuario</td><td><input type="text" name="usernameTrabajador" value="<?php echo $_SESSION['username']?>" disabled></td></tr>
+            <tr><td>Nombre</td><td><input type="text" name="nombreTrabajador" value="<?php echo $_SESSION['nombreTrabajador']?>" pattern="^[a-zA-Z\s]+$" title="Nombre incorrecto"></td></tr>
+            <tr><td>Apellidos</td><td><input type="text" name="apellidosTrabajador" value="<?php echo $_SESSION['apellidosTrabajador']?>" pattern="^[a-zA-Z\s]+$" title="Apellidos incorrectos"></td></tr>
+            <tr><td>Nueva Contraseña</td><td><input type="password" name="passwordTrabajador"></td></tr>
+          </tbody>
+        </table>
+        <div class="input-group">
+          <button type="submit" class="btn2" name="mod_admin">Modificar</button>
+          <button type="submit" class="btn4" name="del_admin" onclick="el()">Eliminar Cuenta</button>
+        </div>
+      </form>
+    </div>
     </div>
     <footer>
         <p>Proyecto Desarrollo Web Entorno Servidor - Rubén Herrera Marcos</p>
     </footer>
 </body>
+<script>
+  function el(){
+    alert("Cuenta Eliminada");
+  }
+</script>
 </html>
--- proyecto/admins/server.php
+++ proyecto/admins/server.php
@@ -5,7 +5,7 @@ $username = "";
 $errors = array(); 
 
 // CONEXION BASE DE DATOS
-$db = mysqli_connect('localhost', 'rubens', 'toor', 'scruben');
+$db = mysqli_connect('localhost', 'ruben', 'toor', 'scruben');
 
 // REGISTRO TRABAJADORES
 if (isset($_POST['reg_user'])) {
@@ -33,7 +33,10 @@ if (isset($_POST['reg_user'])) {
   	$query = "INSERT INTO trabajador (nombre_trabajador, usuario_trabajador, pass_trabajador, apellidos_trabajador) 
                 VALUES ('$nombre','$username','$password','$apellidos');";
   	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
+    $_SESSION['username'] = $username;
+    $_SESSION['passwordTrabajador'] = $password;
+    $_SESSION['nombreTrabajador']=$nombre;
+    $_SESSION['apellidosTrabajador']=$apellidos;
   	$_SESSION['success'] = "Trabajador Registrado";
   	header('location: index.php');
   }
@@ -50,13 +53,46 @@ if (isset($_POST['login_user'])) {
       }
         $query = "SELECT * FROM trabajador WHERE usuario_trabajador='$username' AND pass_trabajador='$password'";
         $results = mysqli_query($db, $query);
+        $datos = mysqli_fetch_array($results);
         if (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
+          $_SESSION['passwordTrabajador'] = $datos[3];
+          $_SESSION['nombreTrabajador']=$datos[1];
+          $_SESSION['apellidosTrabajador']=$datos[4];
           $_SESSION['success'] = "Sesion Iniciada Correctamente";
           header('location: index.php');
         }else {
             array_push($errors, "Error Usuario/Contraseña");
         }
+    }
+  }
+  //MODIFICAR ADMIN
+  if (isset($_POST['mod_admin'])) {
+    // RECEPCION DATOS DE PERFILCLIENTE.PHP
+    $username = $_SESSION['username'];
+    $password = mysqli_real_escape_string($db, $_POST['passwordTrabajador']);
+    $nombre = mysqli_real_escape_string($db, $_POST['nombreTrabajador']);
+    $apellidos = mysqli_real_escape_string($db, $_POST['apellidosTrabajador']);
+    if($password!=""){
+        $password = md5($password);//ENCRIPTADO DE LA CONTRASEÑA
+  
+        $query = "UPDATE trabajador 
+        SET pass_trabajador='$password', nombre_trabajador='$nombre', apellidos_trabajador='$apellidos'
+        WHERE usuario_trabajador='$username';";
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['nombreTrabajador'] = $nombre;
+        $_SESSION['apellidosTrabajador'] = $apellidos;
+        $_SESSION['success'] = "Modificado Correctamente";
+    }else{
+        $query = "UPDATE trabajador 
+        SET  nombre_trabajador='$nombre', apellidos_trabajador='$apellidos'
+        WHERE usuario_trabajador='$username';";
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['nombreTrabajador'] = $nombre;
+        $_SESSION['apellidosTrabajador'] = $apellidos;
+        $_SESSION['success'] = "Modificado Correctamente";
     }
   }
   
